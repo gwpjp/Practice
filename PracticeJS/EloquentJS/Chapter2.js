604024b2036693bfe7e99d912bf9c15adfbbdb00
John Pearson <jpearson@john-pearson-macbook-pro.local>
2016-01-26 15:38:03 -0500

    Working through Chapter 4 in Eloquent JS.

--- PracticeJS/EloquentJS/Chapter2.js
+++ PracticeJS/EloquentJS/Chapter2.js
@@ -5,49 +5,55 @@
 "use strict";
 
 //Looping a Triangle
-console.log("Looping a Triangle");
-var text = "";
-for (var i=0; i < 7; i++) {
-  text = text + "#";
-  console.log(text);
-           }
+ var triangleLoop = function () {
+  console.log("Looping a Triangle");
+  var text = "";
+  for (var i=0; i < 7; i++) {
+    text = text + "#";
+    console.log(text);
+             }
+};  
 
 //FizzBuzz
-console.log("");
-console.log("FizzBuzz");
-var text = "";
-for (var i=1; i < 100; i++) {
-  if (i%3 === 0) {
-    if (i%5 === 0){
-      text += "FizzBuzz,";
+var FizzBuzz = function () {
+  console.log("");
+  console.log("FizzBuzz");
+  var text = "";
+  for (var i=1; i < 100; i++) {
+    if (i%3 === 0) {
+      if (i%5 === 0){
+        text += "FizzBuzz,";
+      }
+      else {
+        text += "Fizz,";
+      }
+    }
+    else if (i%5 === 0){
+      text += "Buzz,";
     }
     else {
-      text += "Fizz,";
+      text= text + i + ",";
     }
   }
-  else if (i%5 === 0){
-    text += "Buzz,";
-  }
-  else {
-    text= text + i + ",";
-  }
-}
-console.log(text);
+  console.log(text);
+};
 
 //Chess Board
-console.log("");
-console.log("Chess Board");
-var size = 8;
-var text = "";
-for (var i=0; i < size; i++) {
-  for (var j=0; j < size; j++) {
-    if ((i+j)%2 === 0) {
-      text += " ";
+var chessBoard = function () {
+  console.log("");
+  console.log("Chess Board");
+  var size = 8;
+  var text = "";
+  for (var i=0; i < size; i++) {
+    for (var j=0; j < size; j++) {
+      if ((i+j)%2 === 0) {
+        text += " ";
+      }
+      else {
+        text += "#";
+      }
     }
-    else {
-      text += "#";
-    }
+    text += "\n";
   }
-  text += "\n";
-}
-console.log(text);
+  console.log(text);
+};
