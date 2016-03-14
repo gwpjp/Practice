e3bf3dbbc3caece5541b2aa0c901a133584c5790
John Pearson <jpearson@john-pearson-macbook-pro.local>
2016-03-14 14:45:15 -0400

    Finished Pizza Hut #1

--- PracticeR/PizzaHut.R
+++ PracticeR/PizzaHut.R
@@ -15,10 +15,42 @@ for (i in c(1,3,7,9)) {
               if (z %% 4 == 0) {
                 z <- as.numeric(paste(z,5,sep="")) #This is now the 5 digit number
                 for (m in c(2,4,6,8)) {
-                  if (m != as.numeric(substr(z,1,1)) && m != as.numeric(substr(z,3,3)) ) {
+                  if (m != as.numeric(substr(z,2,2)) && m != as.numeric(substr(z,4,4)) ) {
                     u <- as.numeric(paste(z,m,sep=""))
                     if (u %% 6 == 0) {
-                      print(u)                      
+                      for (n in c(1,3,7,9)) {
+                        if (n != as.numeric(substr(u,1,1)) && n != as.numeric(substr(u,3,3)) ) {
+                          w <- as.numeric(paste(u,n,sep=""))
+                          if (w %% 7 == 0) {
+                            for (o in c(2,4,6,8)) {
+                              if (o != as.numeric(substr(w,2,2)) && o != as.numeric(substr(w,4,4)) && o != as.numeric(substr(w,6,6)) ) {
+                                t <- as.numeric(paste(w,o,sep=""))
+                                if (t %% 8 == 0) {
+                                  for (p in c(1,3,7,9)) {
+                                    if (p != as.numeric(substr(t,1,1)) && p != as.numeric(substr(t,3,3)) && p != as.numeric(substr(t,7,7)) ) {
+                                      s <- as.numeric(paste(t,p,sep=""))
+                                      if (s %% 9 == 0) {
+                                        print(s)
+                                        
+                                        
+                                        
+                                        
+                                      }       
+                                    }
+                                  }
+                                  
+                                  
+                                  
+                                }       
+                              }
+                            }
+                            
+                            
+                            
+                          }       
+                        }
+                      }                  
+                      
                       
                       
                       
