f800f2dbc91a39e624048105c7fcf8aae506b2e1
John Pearson <jpearson@john-pearson-macbook-pro.local>
2016-05-18 12:52:53 -0400

    Working through Chapter 8

--- PracticeJS/EloquentJS/Chapter8.js
+++ PracticeJS/EloquentJS/Chapter8.js
@@ -4,6 +4,35 @@
 "use strict";
 
 //Retry
-function primitiveMultiply(x,y) {
-  
+function MultiplicatorUnitFailure() {
+  this.message = "The odds are not in your favor!";
+  this.stack = (new Error()).stack;
 }
+
+MultiplicatorUnitFailure.prototype = Object.create(Error.prototype);
+MultiplicatorUnitFailure.prototype.name = "MultiplicatorUnitFailure";
+
+function primitiveMultiply(x,y) {
+  var r = Math.floor(2*Math.random());
+  
+  if (r === 0) {
+    console.log(x*y);
+  } else {
+    throw new MultiplicatorUnitFailure();
+  }
+};
+
+for (;;) {
+  try {
+    primitiveMultiply(1,2);
+    break;
+  } catch (e) {
+    if (e instanceof MultiplicatorUnitFailure) {
+      console.log(e.message);
+    } else {
+      throw e;
+    }
+  }
+
+}
+
