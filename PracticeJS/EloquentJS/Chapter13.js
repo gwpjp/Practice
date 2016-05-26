25b4955cadfcec96be45b1d2ba2347fac8d11afa
John Pearson <john.pearson.02@gmail.com>
2016-05-26 10:05:54 -0400

    Finished Chapter 13

--- PracticeJS/EloquentJS/Chapter13.js
+++ PracticeJS/EloquentJS/Chapter13.js
@@ -6,6 +6,7 @@
 //Build a table given an array of objects
 function buildTable(arr) {
 	var table = document.createElement("table");
+	table.id = "myTable";
 	var headrow = document.createElement("tr");
 
 	//Find keys of first element and append to header row
@@ -22,10 +23,58 @@ function buildTable(arr) {
 		for (var key in elt) {
 			let col = document.createElement("td");
 			col.appendChild(document.createTextNode(elt[key]));
+			if (typeof elt[key] === 'number') {
+				col.style.textAlign = "right";
+			}
 			row.appendChild(col);
 		}
 		table.appendChild(row);
 	}
 
 	document.body.appendChild(table);
+}
+
+//Elements by Tag Name
+function getElements(node,tag) {
+	var children = node.childNodes;
+	var arr = [];
+
+	for (var elt in Object.keys(children)) {
+		if (children[elt].tagName.toLowerCase() === tag.toLowerCase()) {
+			arr.push(children[elt]);
+		}
+	}
+	return arr;
+}
+
+//The cat's hat
+function catFun() {
+	var html = `
+		<p  style="text-align: center">
+			<img id="cat" src="cat.png" style="position: absolute">
+		</p>
+		<p  style="text-align: center">
+			<img id="hat" src="hat.png" style="position: absolute">
+		</p>
+	`;
+
+	document.body.innerHTML = html;
+
+	var cat = document.getElementById("cat"); 
+	var angle = 0, lastTime = null;
+	var hat = document.querySelector("#hat"); 
+
+
+	function animate(time) {
+		if (lastTime != null) {
+			angle += (time - lastTime) * 0.001;
+		}
+		lastTime = time;
+		cat.style.top = (Math.sin(angle) * 100) + 200 + "px"; 
+		cat.style.left = (Math.cos(angle) * 200) + 200 + "px"; 
+		hat.style.top = (Math.sin(angle+90) * 100) + 200 + "px"; 
+		hat.style.left = (Math.cos(angle+90) * 200) + 200 + "px";
+		requestAnimationFrame(animate);
+	}
+	requestAnimationFrame(animate);
 }
