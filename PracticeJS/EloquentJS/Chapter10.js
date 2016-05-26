733844e1d0b759b9b8bca74dd57cc37d075f513d
John Pearson <john.pearson.02@gmail.com>
2016-05-26 07:16:10 -0400

    Working on Chapter 10

---
+++ PracticeJS/EloquentJS/Chapter10.js
@@ -0,0 +1,15 @@
+/*jslint browser:true */
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 */
+
+"use strict";
+
+//Month Names
+
+var monthNumbers = function () {
+	var names = ["January", "February", "March", "April", "May", "June", "July", "August", 
+		"September", "October", "November", "December"];
+	return {
+		name: function(num) {return names[num-1];},
+		number: function(name) {return names.indexOf(name)+1;}
+	};
+}();
