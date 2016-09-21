762e5ab3feef268af63436b6e4a520bd2b4df5d2
John Pearson <jpearson@John-Pearson-MacBook-Air.local>
2016-09-21 09:46:21 -0400

    Did some refactoring of the code to make it run faster.

--- PracticeJS/Euler-net/Webpackv2/app/exercises/Ex3.js
+++ PracticeJS/Euler-net/Webpackv2/app/exercises/Ex3.js
@@ -7,12 +7,21 @@ export default function ex3(node) {
   let prime = 0;
   let m = 0;
 
-  for (let i = 1; i < n; i += 1) {
+  for (let i = 1; i < n; i += 2) {
+    primeCheck = 1;
     // First, check to see if 'i' is prime.
-    m = Math.sqrt(n);
-    for (let j = 1; j < m; j += 1) {
-      if (i % j === 0) {
-        primeCheck = j;
+    m = Math.sqrt(i);
+    if (i % 3 === 0) { // i is definitely not even
+      primeCheck = 3;
+    } else {
+      // i is prime iff it is divisible by 6k-1 or 6k+1
+      for (let j = 5; j < m; j += 6) {
+        if (i % j === 0 || i % (j + 2) === 0) {
+          primeCheck = j;
+          if (primeCheck > 1) {
+            break;
+          }
+        }
       }
     }
     /* If it is prime, then its largest divisor is 1,
--- PracticeJS/Euler-net/Webpackv2/webpack.config.js
+++ PracticeJS/Euler-net/Webpackv2/webpack.config.js
@@ -37,7 +37,7 @@ const common = {
       title: 'Euler-net Webpack pt2',
       template: 'template.html'
     })
-  ],
+  ]
 
 };
 
