317563ab9ac0efa83f8710e3c35a2ca08d587b65
John Pearson <john.pearson.02@gmail.com>
2017-01-11 20:18:31 -0500

    Updated all packages to latest version.  In so doing, needed to update code to current standards because of the linter.  I also moved my linter to ECMAv7.

--- PracticeJS/Euler-net/Webpackv2/app/exercises/Ex6.js
+++ PracticeJS/Euler-net/Webpackv2/app/exercises/Ex6.js
@@ -1,10 +1,10 @@
 // Example 6: Sum and squares
 export default function ex6() {
   const N = 100;
-  const sumsq = Math.pow((N / 2) * (N + 1), 2);
+  const sumsq = ((N / 2) * (N + 1)) ** 2;
   let sqsum = 0;
   for (let i = 1; i <= 100; i += 1) {
-    sqsum += Math.pow(i, 2);
+    sqsum += i ** 2;
   }
 
   return sumsq - sqsum;
