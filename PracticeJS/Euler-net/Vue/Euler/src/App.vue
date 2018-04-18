6676eb992f254af81d8698ce3149d90b53f9df1d
John Pearson <john.pearson.02@gmail.com>
2018-04-18 11:40:08 -0400

    Added SASS functionality.

--- PracticeJS/Euler-net/Vue/Euler/src/App.vue
+++ PracticeJS/Euler-net/Vue/Euler/src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <h2>Project Euler</h2>
+
+    <img src="./assets/Leonhard_Euler.jpg" style="width:200px;float:right">
     <router-view/>
   </div>
 </template>
@@ -11,13 +13,20 @@ export default {
 };
 </script>
 
-<style>
+<style lang='scss'>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  $primary-color: magenta;
+
+  font-family: 'Helvetica Neue';
+  font-size: 14px;
+
+  h2 {
+    color: $primary-color;
+  }
+
+  #asdfasdadfa {
+    color: green;
+    font-weight: bold;
+  }
 }
 </style>
