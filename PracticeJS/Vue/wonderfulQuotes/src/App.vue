6cad769dbef759c8d75592fc89d1d4d834eded8c
gwpjp <john.pearson.02@gmail.com>
2018-05-10 13:21:40 -0400

    Added Bootstrap and header bar.

--- PracticeJS/Vue/wonderfulQuotes/src/App.vue
+++ PracticeJS/Vue/wonderfulQuotes/src/App.vue
@@ -1,6 +1,11 @@
 <template>
-  <div id="app">
-    
+  <div id="app" class="container">
+    <h1>Quotes Added</h1>
+    <div class="progress">
+        <div class="progress-bar" role="progressbar"  :style="{ width: progress*10 + '%' }" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100">
+          {{progress}}/10
+        </div>
+    </div>
   </div>
 </template>
 
@@ -9,12 +14,15 @@ export default {
   name: 'app',
   data () {
     return {
-
+      progress: 1
     }
   }
 }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
+  #app {
+    margin: 50px auto;
+  }
 
 </style>
