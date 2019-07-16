358b4c69f79d3fb4e0da2dfa8b6a712b8f768e00
John Pearson <john.pearson.02@gmail.com>
2019-07-16 11:23:45 -0400

    Added Redux Sagas.

--- JS/React/maxSBurger/src/store/actions/index.js
+++ JS/React/maxSBurger/src/store/actions/index.js
@@ -1,16 +1,29 @@
 export {
     addIngredient,
     removeIngredient,
-    initIngredients
+    initIngredients,
+    setIngredients,
+    fetchIngredientsFailed
 } from './burgerBuilder';
 export {
     purchaseBurger,
     purchaseInit,
-    fetchOrders
+    fetchOrders,
+    purchaseBurgerStart,
+    purchaseBurgerFail,
+    purchaseBurgerSuccess,
+    fetchOrdersSuccess,
+    fetchOrdersStart,
+    fetchOrdersFail
 } from './order';
 export {
     auth,
     logout,
     setAuthRedirectPath,
-    authCheckState
+    authCheckState,
+    logoutSucceed,
+    authStart,
+    authSuccess,
+    authFail,
+    checkAuthTimeout
 } from './auth';
